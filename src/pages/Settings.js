99ee2fee74c916efa4a51483bac7d920f9277232
Plsr <christianpoplawski@gmail.com>
2022-06-18 11:40:25 +0200

    Settings UI cleanup

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import { ArrowBackIcon } from '@chakra-ui/icons'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Center } from '@chakra-ui/react'
 
 export default function Settings() {
   return (
-    <>
+    <Flex h="100%" direction="column">
       <Link to='/'><Flex align="center"><ArrowBackIcon /> Back</Flex></Link>
-      <div>Settings</div>
-    </>
+      <Center h="100%">
+        <div>Here be Settings</div>
+      </Center>
+    </Flex>
   )
 }
